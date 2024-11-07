3b5877763ecf02e88bf28620aa8215ebeeb9a5ce
Rakesh Biswas <rbiswas01999@gmail.com>
2024-11-07 10:01:31 +0600

    3.7.ts [statice] added

--- module3/3.7.ts
+++ module3/3.7.ts
@@ -15,6 +15,7 @@
             return (Counter.count = Counter.count - 1);
         }
     }
+    
     // const instance1 = new Counter();
     // console.log(instance1.increament());
     // console.log(instance1.increament());
