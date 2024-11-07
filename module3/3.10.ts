a9dfd3b73a1c028d437f3a20dafa9ae362f84395
Rakesh Biswas <rbiswas01999@gmail.com>
2024-11-07 11:21:43 +0600

    3.10.ts [encapsulation] added

---
+++ module3/3.10.ts
@@ -0,0 +1,42 @@
+{
+    // access modifier 
+    // encapsulation
+    class BankAccount {
+        public readonly id: number;
+        public name: string;
+        // public balance: number; // by-default public
+        private _balance: number; // if we make it private , then it will be accessible within this class
+
+        constructor(id: number, name: string, balance: number) {
+            this.id = id;
+            this.name = name;
+            this._balance = balance;
+        }
+
+        public addDeposit(amount: number) {
+            this._balance = this._balance + amount
+        }
+
+        // public getBalance(){
+        private getBalance(){
+            return this._balance;
+        }
+
+        getHiddenMethod(){
+            return this.getBalance();
+        }
+    }
+
+    class StudentAccount extends BankAccount{
+        test(){
+            this.
+        }
+    }
+
+    const goribManusherAccount = new BankAccount(111, "Mr. Gorib", 20);
+    // goribManusherAccount.balance =0 ;
+    goribManusherAccount.addDeposit(20);
+    const myBalance = goribManusherAccount.getBalance();
+    console.log(myBalance);
+    // 
+}
