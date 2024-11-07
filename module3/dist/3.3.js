b1ca1bb19658d58c0609de1b125f382aab8058c2
Rakesh Biswas <rbiswas01999@gmail.com>
2024-11-07 08:45:24 +0600

    3.4.ts [instanceof guard] added

--- module3/dist/3.3.js
+++ module3/dist/3.3.js
@@ -9,7 +9,26 @@
             return param1.toString() + param2.toString();
         }
     };
-    var result = add(45, 5);
-    console.log(result);
+    var result1 = add("45", "50");
+    console.log(result1);
+    var getUser = function (user) {
+        // user.  // gives --> only name(property)
+        if ('role' in user) {
+            // user.  // gives --> both name(property), age(property)
+            console.log("My name is ".concat(user.name, " and my role is ").concat(user.role));
+        }
+        else {
+            console.log("My name is ".concat(user.name));
+        }
+    };
+    var normalUser = {
+        name: "Mr. Normal User"
+    };
+    var adminUser = {
+        name: "Mr. Admin User",
+        role: "admin"
+    };
+    getUser(normalUser);
+    getUser(adminUser);
     // 
 }
